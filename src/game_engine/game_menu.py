16d3f34510b9dbf6d286fa404ac7c9a4609ae9ab
KubaTaba1uga <jakub@buczynski.net.pl>
2023-06-01 09:47:22 +0200

    add fields map

--- src/game_engine/game_menu.py
+++ src/game_engine/game_menu.py
@@ -1,17 +1,50 @@
+from copy import deepcopy
+
 from src.constants import GAME_MENU_CTX
 
-_MENU_FIELDS_STATUS_MAP = {
-    "create_new_session": {
-        "display_name": "Start New Game",
-        "selected": False,
-        "bounded_ctx": GAME_MENU_CTX.CREATE_NEW_SESSION,
-        "disabled": False,
+_MENU_FIELDS_MAP = {
+    GAME_MENU_CTX.MENU: {
+        "title": "Game Menu",
+        "fields": {
+            "create_new_session": {
+                "display_name": "Start New Game",
+                "selected": False,
+                "next_ctx": GAME_MENU_CTX.CREATE_NEW_SESSION,
+                "disabled": False,
+            },
+            "save_current_session": {
+                "display_name": "Start New Game",
+                "selected": False,
+                "next_ctx": GAME_MENU_CTX.SAVE_CURRENT_SESSION,
+                "disabled": False,
+            },
+        },
     },
-    "save_current_session": {
-        "display_name": "Start New Game",
-        "selected": False,
-        "bounded_ctx": GAME_MENU_CTX.CREATE_NEW_SESSION,
-        "disabled": False,
+    GAME_MENU_CTX.CREATE_NEW_SESSION: {
+        "title": "Board Choice",
+        "fields": {
+            # this part should be generated dynamically based on BoardABS children.
+            # this is only ex.
+            "choose_board": {
+                "display_name": "Board Choice",
+                "selected": False,
+                "next_ctx": GAME_MENU_CTX.CHOOSE_DIFFICULTY,
+                "disabled": False,
+            },
+        },
+    },
+    GAME_MENU_CTX.CHOOSE_DIFFICULTY: {
+        "title": "Difficulty Choice",
+        "fields": {
+            # this part should be generated based on DifficultyABS children.
+            # this is only ex.
+            "choose_difficulty": {
+                "display_name": "Difficulty Choice",
+                "selected": False,
+                "next_ctx": GAME_MENU_CTX.CHOOSE_DIFFICULTY,
+                "disabled": False,
+            },
+        },
     },
 }
 
